6dd339a43a704320ce55e0349015f6a03d4e855e
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-09-17 19:56:11 +0300

    Add the variables for the Car-Rental class

--- booking-app-ui/src/app/models/car-rental/car-rental.ts
+++ booking-app-ui/src/app/models/car-rental/car-rental.ts
@@ -1,2 +1,34 @@
+import { Time } from "@angular/common"
+
 export class CarRental {
+  bookingId ?: number
+  vehicleId ?: number
+  totalCost ?: number
+  numberOfPassengers ?: number
+  numberOfDoors ?: number
+  pricePerDay ?: number
+  vehicleValue ?: number
+  rentalDays ?: number
+
+  startDate ?: Date
+  endDate ?: Date
+
+  startTime ?: Time
+  endTime ?: Time
+
+  customerName ?: string
+  customerEmail ?: string
+  customerPhoneNumber ?: string
+  customerLicenseNumber ?: string
+  customerPhysicalAddress ?: string
+  insuranceType ?: string
+
+  pickUpLocation ?: string
+  dropOffLocation ?: string  
+
+  registrationNumber ?: string
+  vehicleType ?: string
+  vehicleImage ?: string
+  transmissionType ?: string
+  gvm ?: string
 }
