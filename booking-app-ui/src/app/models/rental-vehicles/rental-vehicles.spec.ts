78e44a8909f460aab43a6945fd407dea024d9205
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-09-17 19:43:59 +0300

    Add the Models directory and rental-vehicles files.
    Added models and rental directory with the files representing the model
    Rental-Vehicles.

---
+++ booking-app-ui/src/app/models/rental-vehicles/rental-vehicles.spec.ts
@@ -0,0 +1,7 @@
+import { RentalVehicles } from './rental-vehicles';
+
+describe('RentalVehicles', () => {
+  it('should create an instance', () => {
+    expect(new RentalVehicles()).toBeTruthy();
+  });
+});
