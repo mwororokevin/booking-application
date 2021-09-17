551d6a7b292af3ae65f6629925b0ec0f8e846266
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-09-17 21:14:51 +0300

    Add methods and variables to the file.
    Add the carRentalService parameter to the constructor, the
    setBookingData, getBookingData and saveBooking data methods.

--- booking-app-ui/src/app/shared/car-rental/car-rental-shared.service.ts
+++ booking-app-ui/src/app/shared/car-rental/car-rental-shared.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CarRental } from 'src/app/models/car-rental/car-rental';
+import { CarRentalService } from 'src/app/services/car-rental-service/car-rental.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarRentalSharedService {
+  carRental : CarRental = new CarRental()
 
-  constructor() { }
+  constructor(private carRentalService : CarRentalService) { }
+
+  setBookingData(data : CarRental) {
+    this.carRental = data
+  }
+
+  getBookingData() : CarRental {
+    return this.carRental
+  }
+
+  saveBooking() {
+    // this.makeBookingService.createBooking(this.booking).subscribe(data => {
+    //   console.log(data)
+    // },
+    // error => console.log(error)
+    // )
+
+    console.log(this.carRental)
+  }
 }
