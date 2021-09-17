7113fe0de8db54379f984a5d4a062a2de88b5222
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-09-17 20:14:36 +0300

    Add Page Components to the declarations section.
    These include the following: HomePageComponent, HeaderSectionComponent, FooterSectionComponent, ContactPageComponent and the AboutUsPageComponent.

--- booking-app-ui/src/app/app.module.ts
+++ booking-app-ui/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
+import { FooterSectionComponent } from './components/footer-section/footer-section.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { AboutUsPageComponent } from './components/about-us-page/about-us-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderSectionComponent,
+    FooterSectionComponent,
+    ContactPageComponent,
+    AboutUsPageComponent
   ],
   imports: [
     BrowserModule
