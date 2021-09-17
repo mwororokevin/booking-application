4e6f6ba8cdef68acc17e83ba971e6749c3174384
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-09-17 20:00:02 +0300

    Add the variables for the Rental-Vehicles class.

--- booking-app-ui/src/app/models/rental-vehicles/rental-vehicles.ts
+++ booking-app-ui/src/app/models/rental-vehicles/rental-vehicles.ts
@@ -1,2 +1,13 @@
 export class RentalVehicles {
+  vehicleId ?: number
+  numberOfPassengers ?: number
+  numberOfDoors ?: number
+  pricePerDay ?: number
+  vehicleValue ?: number
+
+  registrationNumber ?: string
+  vehicleType ?: string
+  vehicleImage ?: string
+  transmissionType ?: string
+  gvm ?: string
 }
